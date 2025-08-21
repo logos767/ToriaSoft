748ec567f846d2d5bb3dd25f5df74eb1cb26de05
datosdonvic <datosdonvic@gmail.com>
2025-08-21 17:28:24 -0400

    corr

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ from app import create_app, socketio
 app = create_app()
 
 if __name__ == '__main__':
-    # This is for local development.
-    # The 'app' object is used by Gunicorn in production.
+    # This file is intended for local development only.
+    # For production, use a WSGI server like Gunicorn pointing to wsgi.py.
     socketio.run(app, debug=True)
