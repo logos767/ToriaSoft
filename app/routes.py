2e21d2293fd1271b95336808811b16590e050219
datosdonvic <datosdonvic@gmail.com>
2025-08-28 17:01:13 -0400

    excel

--- app/routes.py
+++ app/routes.py
@@ -903,12 +903,12 @@ def cargar_excel():
                     continue
                 
                 barcode = str(row[0]).strip()
+                codigo_producto = str(row[6]).strip() if len(row) > 6 and row[6] is not None else ''
                 name = str(row[1]).strip()
                 cost_usd = row[2] if row[2] is not None else 0
                 price_usd = row[3] if row[3] is not None else 0
                 stock = row[4] if row[4] is not None else 0
                 image_url = row[5] if row[5] is not None else ''
-                codigo_producto = str(row[6]).strip() if len(row) > 6 and row[6] is not None else ''
                 marca = str(row[7]).strip() if len(row) > 7 and row[7] is not None else ''
                 color = str(row[8]).strip() if len(row) > 8 and row[8] is not None else ''
                 talla = str(row[9]).strip() if len(row) > 9 and row[9] is not None else ''
@@ -918,6 +918,8 @@ def cargar_excel():
                 if product:
                     updates.append({
                         'id': product.id,
+                        'new_codigo_producto': codigo_producto,
+                        'old_codigo_producto': product.codigo_producto,
                         'name': product.name,
                         'new_stock': int(stock),
                         'old_stock': product.stock,
@@ -926,8 +928,6 @@ def cargar_excel():
                         'new_name': name,
                         'new_price_usd': float(price_usd),
                         'new_image_url': image_url,
-                        'new_codigo_producto': codigo_producto,
-                        'old_codigo_producto': product.codigo_producto,
                         'new_marca': marca,
                         'old_marca': product.marca,
                         'new_color': color,
@@ -938,12 +938,12 @@ def cargar_excel():
                 else:
                     new_products.append(Product(
                         barcode=barcode,
+                        codigo_producto=codigo_producto,
                         name=name,
                         cost_usd=float(cost_usd),
                         price_usd=float(price_usd),
                         stock=int(stock),
                         image_url=image_url,
-                        codigo_producto=codigo_producto,
                         marca=marca,
                         color=color,
                         size=talla
